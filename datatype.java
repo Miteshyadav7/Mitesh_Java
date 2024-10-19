0bd5f58f867fbd087eac24876910c039da6d888b
Miteshyadav7 <167742972+Miteshyadav7@users.noreply.github.com>
2024-10-19 15:21:35 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,20 @@
+class hello {
+    public static void main (String[] args){
+byte a=10;
+short b=20;
+int c=1000;
+long d=200;
+char e=M;"M"
+float f=25.52f;
+String g="mitesh";
+double h=2544640.5454;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println(g);
+System.out.println(h);
+}
+}
