42560e5fb3a2fc3d568e7bd4945066fdc298a288
Miteshyadav7 <167742972+Miteshyadav7@users.noreply.github.com>
2024-10-19 15:24:04 +0530

    Create perimeter.java

---
+++ perimeter.java
@@ -0,0 +1,12 @@
+class perimeter{
+public static void main(String [] args){
+int l;
+
+int perimeter = 60;
+l = perimeter/6;
+int width = 2*l;
+                                                                               
+System.out.println(l);
+System.out.println(width);
+}
+}
